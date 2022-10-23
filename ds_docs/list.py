0fbf15b88c890edf2313a04c8e8bf5f9804f6d2a
MoAyoub <medayb45esi@gmail.com>
2022-10-23 09:31:50 +0100

    fixed

---
+++ ds_docs/list.py
@@ -0,0 +1,41 @@
+# Creating a List with
+# the use of multiple values
+List = ["Geeks", "For", "Geeks"]
+print("\nList containing multiple values: ")
+print(List)
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List2 = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti-Dimensional List: ")
+print(List2)
+
+# accessing a element from the
+# list using index number
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+# accessing a element using
+# negative indexing
+print("Accessing element using negative indexing")
+
+# print the last element of list
+print(List[-1])
+
+# print the third last element of list
+print(List[-3])
+
+
+# ! output
+
+##
+# *Creating Dictionary:
+# *{'Name': 'Geeks', 1: [1, 2, 3, 4]}
+# * Accessing a element using key:
+# * Geeks
+# * Accessing a element using get:
+# * [1, 2, 3, 4]
+# * {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+# #
