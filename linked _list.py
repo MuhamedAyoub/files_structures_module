3e0853ab84743419101d83c664aef7c3d9c3da13
MoAyoub <medayb45esi@gmail.com>
2022-10-23 09:29:51 +0100

    done

---
+++ linked _list.py
@@ -0,0 +1,82 @@
+"""A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations. The elements in a linked list are linked using pointers
+
+
+
+    """
+
+
+#! Declaration
+# Node class
+class Node:
+
+    # Function to initialize the node object
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize
+        # next as null
+
+# Linked List class
+
+
+class LinkedList:
+
+    # Function to initialize the Linked
+    # List object
+    def __init__(self):
+        self.head = None
+
+
+""""
+How to  create linked list 
+
+"""
+# Code execution starts here
+if __name__ == '__main__':
+
+    # Start with the empty list
+    llist = LinkedList()
+
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    '''
+    Three nodes have been created.
+    We have references to these three blocks as head,
+    second and third
+ 
+    llist.head     second             third
+        |             |                 |
+        |             |                 |
+    +----+------+     +----+------+     +----+------+
+    | 1 | None |     | 2 | None |     | 3 | None |
+    +----+------+     +----+------+     +----+------+
+    '''
+
+    llist.head.next = second  # Link first node with second
+
+    '''
+    Now next of first Node refers to second. So they
+    both are linked.
+ 
+    llist.head     second             third
+        |             |                 |
+        |             |                 |
+    +----+------+     +----+------+     +----+------+
+    | 1 | o-------->| 2 | null |     | 3 | null |
+    +----+------+     +----+------+     +----+------+
+    '''
+
+    second.next = third  # Link second node with the third node
+
+    '''
+    Now next of second Node refers to third. So all three
+    nodes are linked.
+ 
+    llist.head     second             third
+        |             |                 |
+        |             |                 |
+    +----+------+     +----+------+     +----+------+
+    | 1 | o-------->| 2 | o-------->| 3 | null |
+    +----+------+     +----+------+     +----+------+
+    '''
